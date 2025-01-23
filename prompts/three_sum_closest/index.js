c36bf14bd9f6e72942729254f138e4f1a977cd3f
Cory Asato <czasato@gmail.com>
2025-01-22 19:55:01 -0800

    Add a faster version of three_sum_closest w/ benchmarks.

--- prompts/three_sum_closest/index.js
+++ prompts/three_sum_closest/index.js
@@ -16,8 +16,60 @@
 // Output: 0
 // Explanation: The sum that is closest to the target is 0. (0 + 0 + 0 = 0).
 
+// Strategy using 3 pointers.
+//
+// jsbench                   - 147M ops/s ± 4.5% Fastest
+// https://www.bigocalc.com/ - Time: O(n^2) | Space: O(1)
+export const threeSumClosestLinear = (nums, target) => {
+  const endIdx = nums.length-1;
+
+  let left = 0;
+  let mid = 1;
+  let right = 2;
+
+  let loop = true;
+  let result = [Math.pow(10, 4), 0];
+
+  while(loop) {
+    if (left > nums.length-3) {
+      loop = false;
+      break;
+    }
+
+    const sum = nums[left] + nums[mid] + nums[right];
+
+    if (sum === target) {
+      result = [0, sum];
+      break;
+    }
+
+    const delta = Math.abs(target - sum);
+    if (delta < result[0]) {
+      result = [delta, sum];
+    }
+
+    if (right < endIdx) {
+      right += 1;
+    } else if (right === endIdx && mid === endIdx-1) {
+      left += 1;
+      mid = left + 1;
+      right = mid + 1;
+    } else {
+      mid += 1;
+      right = mid + 1;
+    }
+  }
+
+  return result[1];
+};
+
 // Utilizes a recursive function to sum all the 3 [i]th elements in a combinatory fashion.
 // The result tuple is to reduce another map we'd have to do if we had saved all the sums. The shortest delta is the closest sum to target.
+//
+// jsbench                   - 123M ops/s ± 6.79% 16.02 % slower
+// https://www.bigocalc.com/ - Time: O(2^n) | Space: O(n)
+//                             Overall, the function is not efficient for large input sizes due to its exponential time complexity,
+//                             and it uses linear space for the recursion stack.
 const threeSumClosest = (nums, target) => {
   let answer = null;
   let result = [Math.pow(10, 4), 0];  // Target constraint is 10,000. Set at max delta from 1.
@@ -53,5 +105,3 @@ const threeSumClosest = (nums, target) => {
 };
 
 export default threeSumClosest;
-
-
--- prompts/three_sum_closest/index.test.js
+++ prompts/three_sum_closest/index.test.js
@@ -1,5 +1,5 @@
 import { describe, expect, test } from "bun:test";
-import threeSumClosest from './index';
+import threeSumClosest, {threeSumClosestLinear} from './index';
 
 const cases = [
   [[-1, 2, 1, 4], 4, 4],
@@ -12,5 +12,11 @@ const cases = [
 describe("three_sum_closest", () => {
   test.each(cases)("%p should contain value %p", (a,b,  expected) => {
     expect(threeSumClosest(a, b)).toBe(expected);
+  });
+});
+
+describe("threeSumClosestLinear", () => {
+  test.each(cases)("%p should contain value %p", (a,b,  expected) => {
+    expect(threeSumClosestLinear(a, b)).toBe(expected);
   });
 });
