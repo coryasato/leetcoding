68e4960358c54403fe0c996bc0a2c6a84e22c4da
Cory Asato <czasato@gmail.com>
2025-02-15 19:31:41 -0800

    Learn the O(n) approach to firstMissingPositive.

--- prompts/41-first-missing-positive/index.js
+++ prompts/41-first-missing-positive/index.js
@@ -19,10 +19,10 @@
 // Output: 1
 // Explanation: The smallest positive integer 1 is missing.
 
-// TODO:
-// This is currently a quatratic time function. The bubble sort top half of the fn needs to be changed to something faster.
+// NOTE:
+// This is a quatratic time function. The bubble sort top half of the fn needs to be changed to something faster.
 // Space complexity is O(1).
-const firstMissingPositive = (nums) => {
+const _firstMissingPositive = (nums) => {
   for (let i = 0; i < nums.length; i++) {
     const num = nums[i];
 
@@ -56,6 +56,48 @@ const firstMissingPositive = (nums) => {
   }
 
   return answer;
+};
+
+// NOTE:
+// Had to look this one up!
+// 1) Flip all negative and out of bound integers to zero. OOB past the length of the array will be ints we don't care about.
+//    Example: [50,1,0,3,10] => We know there are smaller numbers that may or may not exist than 50 or 10.
+//    Example: [7,8,9]       => We know that the answer here is 1.
+//
+// 2) This is the clever part; We utilize an integer as a constant index. So the same integer will always have the same index as we mutate
+//    the initial array. In this way, we can say, "index ZERO will always be where integer ONE exists in the array". If we have seen integer ONE,
+//    then index ZERO will be incremented by its current value PLUS the length of the array.
+//    Example: [1,2,3]       => Results to [5, 6, 7].
+//    Example: [3,2,1]       => Results to [7, 6, 5].
+//    Example: [3,0,1]       => Results to [7, 0, 5].
+//
+// 3) Lastly, we find the first index that is less than the length of the array and increment by one.
+//    Example: [3,0,1]       => Results to [7, 0, 5] | This is saying, we haven't seen index 1 since its less than the length of the array (3) and this is where integer 2 should live.
+
+// Time: O(n) | Space: O(1)
+const firstMissingPositive = (nums) => {
+  let len = nums.length;
+
+  for (var i = 0; i < nums.length; i++) {
+    if (nums[i] < 0 || nums[i] > len) {
+      nums[i] = 0;
+    }
+  }
+
+  for (var i = 0; i < nums.length; i++) {
+    if (nums[i] % (len + 1) <= len) {
+      const idx = ((nums[i] % (len + 1)) - 1);
+      nums[idx] += (len+1);
+    }
+  }
+
+  for (var i = 0; i < nums.length; i++) {
+    if (nums[i] <= len) {
+      return i + 1;
+    }
+  }
+
+  return len + 1;
 };
 
 export default firstMissingPositive;
