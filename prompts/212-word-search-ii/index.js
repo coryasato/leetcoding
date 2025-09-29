d352dee8d4b0530886048391ba7934144b8af871
Cory Asato <czasato@gmail.com>
2025-09-29 11:17:57 -0700

    212: Adds logic to disallow using the same cell more than once per word.

--- prompts/212-word-search-ii/index.js
+++ prompts/212-word-search-ii/index.js
@@ -12,20 +12,25 @@
 // Input: board = [["a","b"],["c","d"]], words = ["abcb"]
 // Output: []
 
+// Tokenizes Row + Column for caching a cell that has been visited.
+const _getCoordsToken = (y, x) => `${y}-${x}`;
+
 const findWords = (board, words) => {
   // Creates a map of each letter to its x,y coordinates in the matrix. Each letter can have many coordinates.
-  // letterMap = { "letter": [ [x,y], [<INT>,<INT>], ... ]}
+  // The third item is an array to host visited cells with their coordinates tokenized.
+  // letterMap = { "letter": [ [x, y, [SEEN TOKENS]], [<INT>,<INT>,[<STRING>]] ]}
   const letterMap = {};
   board.forEach((row, i) => {
     row.forEach((letter, j) => {
-      letterMap[letter] = (letterMap[letter] || []).concat([[i, j]]);
+      letterMap[letter] = (letterMap[letter] || []).concat([[i, j, [] ]]);
     });
   });
 
   // For each word, find all possible coordinates of the first letter.
   // For each letter coordinates, we look directly in the board (O(1)) to see if we can can travel to the next letter.
   // If we can, we create a new array of coordinates for the next letter, filtering out any current coordinates that
-  // does not have an adjacent letter cell.
+  // does not have an adjacent letter cell. The seen array must be recreated per nextMatches entry because a particular
+  // cell can and might branch into many directions if an adjacent cell for a letter matches more than once.
   const res = [];
   words.forEach(word => {
     let idx = 0;
@@ -37,21 +42,26 @@ const findWords = (board, words) => {
       const nextMatches = [];
 
       matches.forEach(pos => {
+        const seen = pos[2];
         const x = pos[1];
         const y = pos[0];
+        const coords = _getCoordsToken(y, x);
 
-        if (board[y-1] && board[y-1][x] === nextLetter) {
+        if (board[y-1] && board[y-1][x] === nextLetter && !seen.includes(_getCoordsToken(y-1, x))) {
           // UP
-          nextMatches.push([y-1, x]);
-        } else if (board[y][x+1] && board[y][x+1] === nextLetter) {
+          nextMatches.push([y-1, x, [...seen, coords]]);
+        }
+        if (board[y][x+1] && board[y][x+1] === nextLetter && !seen.includes(_getCoordsToken(y, x+1))) {
           // RIGHT
-          nextMatches.push([y, x+1]);
-        } else if (board[y+1] && board[y+1][x] === nextLetter) {
+          nextMatches.push([y, x+1, [...seen, coords]]);
+        }
+        if (board[y+1] && board[y+1][x] === nextLetter && !seen.includes(_getCoordsToken(y+1, x))) {
           // DOWN
-          nextMatches.push([y+1, x]);
-        } else if (board[y][x-1] && board[y][x-1] === nextLetter) {
+          nextMatches.push([y+1, x, [...seen, coords]]);
+        }
+        if (board[y][x-1] && board[y][x-1] === nextLetter && !seen.includes(_getCoordsToken(y, x-1))) {
           // LEFT
-          nextMatches.push([y, x-1]);
+          nextMatches.push([y, x-1, [...seen, coords]]);
         }
       });
       // Mutate matches for the next letter's coordinates.
--- prompts/212-word-search-ii/index.test.js
+++ prompts/212-word-search-ii/index.test.js
@@ -4,6 +4,9 @@ import findWords from './index';
 const cases = [
   [[["o","a","a","n"],["e","t","a","e"],["i","h","k","r"],["i","f","l","v"]], ["oath","pea","eat","rain"], ["oath","eat"]],
   [[["a","b"],["c","d"]], ["abcb"], []],
+  [[["o","a","a","n"],["e","t","a","e"],["i","h","k","r"],["i","f","l","v"]], ["etae"], ["etae"]],
+  [[["o","a","a","n"],["e","t","a","e"],["i","h","k","r"],["i","f","l","v"]], ["etaea"], []],            // Asserts that visited cells cannot be used more than once
+  [[["o","a","a","n"],["e","t","a","e"],["i","h","k","r"],["i","f","l","v"]], ["etaenaa"], ["etaenaa"]], // Sanity check for the avobe logic
 ];
 
 describe("word-search-ii", () => {
