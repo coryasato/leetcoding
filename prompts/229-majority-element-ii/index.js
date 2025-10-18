4f8ac5c48a1e1d293004201339031a7ea61196a8
Cory Asato <czasato@gmail.com>
2025-10-18 13:12:17 -0700

    229: Add follow up solution via Boyer–Moore algo.

--- prompts/229-majority-element-ii/index.js
+++ prompts/229-majority-element-ii/index.js
@@ -14,7 +14,8 @@
 // Input: nums = [1,2]
 // Output: [1,2]
 
-const majorityElement = (nums) => {
+// O(N) time | O(N) space
+const _majorityElement = (nums) => {
   const floor = Math.floor(nums.length / 3);
   const map = {};
   const res = new Set();
@@ -28,6 +29,53 @@ const majorityElement = (nums) => {
   });
 
   return [...res];
+};
+
+// Follow up: Could you solve the problem in linear time and in O(1) space?
+// Boyer-Moore Voting Algorithm: https://en.wikipedia.org/wiki/Boyer%E2%80%93Moore_majority_vote_algorithm
+// O(N) time | O(1) space
+const majorityElement = (nums) => {
+  let numA = null;
+  let numB = null;
+  let countA = 0;
+  let countB = 0;
+
+  // Find the most repeating entries in the array. This will give us our best 2 (at most) candidates.
+  nums.forEach(num => {
+    if (num === numA) {
+      countA++;
+    } else if (num === numB) {
+      countB++;
+    } else if (countA === 0) {
+      numA = num;
+      countA = 1;
+    } else if (countB === 0) {
+      numB = num;
+      countB = 1;
+    } else {
+      countA = Math.max(0, countA-1);
+      countB = Math.max(0, countB-1);
+    }
+  });
+
+  // Tally the actual count for our candidates.
+  countA = 0;
+  countB = 0;
+  nums.forEach(num => {
+    if (num === numA) {
+      countA++;
+    } else if (num === numB) {
+      countB++;
+    }
+  });
+
+  const res = [];
+  const threshold = Math.floor(nums.length / 3);  // ⌊ n/3 ⌋ (the "⌊ ⌋" chars means floor)
+
+  if (countA > threshold) { res.push(numA); }
+  if (countB > threshold) { res.push(numB); }
+
+  return res;
 };
 
 export default majorityElement;
