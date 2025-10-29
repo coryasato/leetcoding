e90c396fdd3d88653e24ae4cd663d214c4988ce7
Cory Asato <czasato@gmail.com>
2025-10-29 12:43:44 -0700

    239: Add optimized solution.

--- prompts/239-sliding-window-maximum/index.js
+++ prompts/239-sliding-window-maximum/index.js
@@ -21,7 +21,8 @@
 // Input: nums = [1], k = 1
 // Output: [1]
 
-const maxSlidingWindow = (nums, k) => {
+// Initial solution. Improvements would be to not have to slice and reduce for each window.
+const _maxSlidingWindow = (nums, k) => {
   const res = [];
   let left = 0;
   let right = k;
@@ -32,6 +33,48 @@ const maxSlidingWindow = (nums, k) => {
     res.push(max);
     left++;
     right++
+  }
+
+  return res;
+};
+
+// Optimize above to reduce the amount of loops:
+// This strategy should spped things up by a bit. The only edge case where we take a speed hit is
+// the reduce op to find a new currMax in an unknown group. Otherwise we save two loops per iteration.
+const maxSlidingWindow = (nums, k) => {
+  const res = [];
+  let left = 0;
+  let right = k-1;
+
+  // Pre-seed the group with the first k numbers and find its max val.
+  let group = [];
+  let currMax = Number.MIN_VALUE;
+
+  for (let i = 0; i < k; i++) {
+    currMax = Math.max(currMax, nums[i]);
+    group.push(nums[i]);
+  }
+
+  // Push the currMax of the first window group.
+  res.push(currMax);
+
+  while(right < nums.length-1) {
+    left++;
+    right++
+    group.push(nums[right]);
+
+    // Remove the first item in the group to maintain k items.
+    const leftNum = group.shift();
+
+    // If the item being removed equals the currMax, we need to find a new max value within the current
+    // window; Only if the new number coming in is less than the last currMax. Else, set the new max directly.
+    if (leftNum === currMax && nums[right] < currMax) {
+      currMax = group.reduce((acc, num) => Math.max(acc, num), Number.MIN_VALUE);
+    } else {
+      currMax = Math.max(currMax, nums[right]);
+    }
+
+    res.push(currMax);
   }
 
   return res;
