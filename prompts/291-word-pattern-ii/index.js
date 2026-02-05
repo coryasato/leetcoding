3bbacd91fe7908f8e534f1ccbd8bbbd47fb88ade
Cory Asato <czasato@gmail.com>
2026-02-04 19:26:46 -0800

    291: Refactor (WIP)

--- prompts/291-word-pattern-ii/index.js
+++ prompts/291-word-pattern-ii/index.js
@@ -24,9 +24,9 @@
 // length to begin with, we need to try all possible math lengths for each unique token divided by the length of the string.
 
 // TODO: Revist the the upper logic for mathing out the tokens and words length. Looks messy.
-const wordPattern = (pattern, str) => {
+const _wordPattern = (pattern, str) => {
   const uniqueTokens = new Set(pattern.split(''));
-  const wordsPerTokens = pattern.length / uniqueTokens.size;
+  const wordsPerTokens = Math.floor(pattern.length / uniqueTokens.size);
   const uniqueWordsPerTokenWords = str.length / wordsPerTokens;
   const avgWordLength = uniqueWordsPerTokenWords / uniqueTokens.size;
 
@@ -39,8 +39,8 @@ const wordPattern = (pattern, str) => {
     const low = Math.floor(avgWordLength);
     const high = Math.ceil(avgWordLength);
     // Gather all permutations of the range of possible char count per token
-    queue.push([low, high]);
-    queue.push([high, low]);
+    const perms = getPermutationsForArr([low, high]);
+    queue.push(...perms);
   }
 
   let res = false;
@@ -97,7 +97,107 @@ const wordPattern = (pattern, str) => {
   return res;
 };
 
+const wordPattern = (pattern, str) => {
+  // Loop over pattern to find duplicate tokens
+  // > Use a Map to hold duplicate token to str patterns when a match is found, then look for this first. This will handle 3 and onward tokens. <
+  // The first token takes the first letter in str, if subsequent tokens do not match, move the token's cursor forward by each token.
+  // Each unique token takes up minimum of 1 char in str.
+  // When we find a duplicate pattern, we want to move both cursors forward, greedy match as far as we can to assign the string to the token.
+  // Once matched, remove both duplicate tokens from pattern and slice out the string ranges from str.
+  // The main loop is done once pattern is exhausted / empty.
+
+  // We're only interested in processing duplicate indexes first. As non duplicate indexes are arbitary strings.
+
+  let duplicateTokenIdxs = [];
+
+  for (let i = 0; i < pattern.length; i++) {
+    const char = pattern[i];
+
+    const rest = pattern.slice(i+1);
+    if (rest.includes(char)) {
+      duplicateTokenIdxs.push({ token: char, posX: [i, pattern.split('').findIndex((c, idx) => idx !== i && c === char)] });
+      break;
+    }
+  }
+
+  // If there are no duplicate tokens, then any token can be any string as long as one char in str can be assigned to one token, minimum.
+  if (duplicateTokenIdxs.length === 0) return [pattern, str];
+  // If there are no duplicate tokens, then any token can be any string as long as one char in str can be assigned to one token, minimum.
+  //if (duplicateTokenIdxs.length === 0) return pattern.length <= str.length;
+
+  let map = new Map();
+  console.log({duplicateTokenIdxs, pattern, str});
+  // Process duplicates first
+  while (duplicateTokenIdxs.length > 0) {
+    const { token, posX } = duplicateTokenIdxs.shift();
+
+    const cursorAStart = posX[0];
+    const charA = str.slice(cursorAStart, cursorAStart + 1);
+
+    let cursorBStart = -1;
+    let cursorRange = 1;
+    let entries = [];
+
+    // Make sure to account for 1 char in str per token index. So we start the i variable past any non-dupe token.
+    const start = cursorAStart + 1;
+    const end = str.length - ((pattern.length-1) - posX[1]);
+    console.log({t: 'BEFORE LOOP', end, start, what: (pattern.length - posX[1])});
+    for (let i = start; i <= end; i++) {
+      if (cursorBStart < 0 && str[i] === charA) {
+        cursorBStart = i;
+        continue;
+      } else if (cursorBStart > 0) {
+        // From here on we have the first matching dupe, lets greedy match as far as we can.
+        cursorRange++;
+        const strA = str.slice(cursorAStart, cursorAStart + cursorRange);
+        const strB = str.slice(cursorBStart, i+1);
+
+        console.log({strA, strB, cursorRange, i, t: str.length - end});
+
+        const isAtEnd = i === (end-1);
+        if (strA !== strB || isAtEnd) {
+          const entry = isAtEnd
+            ? { str: strA, cursorBStart, cursorRange }
+            : { str: strA.slice(0, -1), cursorBStart, cursorRange: cursorRange-1 };
+          // console.log({t: 'DUMP', entry});
+
+          if (entries.length === 0) {
+            entries.push(entry);
+          } else if (entries.length > 0 && entries[0].cursorRange < entry.cursorRange) {
+            entries = [entry];
+          }
+
+          // Reset vars
+          i = cursorBStart;
+          cursorBStart = -1;
+          cursorRange = 1;
+        }
+      }
+    }
+
+    const bestEntry = entries[0];
+
+    if (bestEntry) {
+      console.log({bestEntry, posX});
+      str = str.slice(0, bestEntry.cursorBStart) + str.slice(bestEntry.cursorBStart + bestEntry.cursorRange);
+      // console.log({t: 1, str});
+      str = str.slice(0, cursorAStart) + str.slice(cursorAStart + bestEntry.cursorRange);
+      pattern = pattern.slice(0, posX[1]) + pattern.slice(posX[1]+1);
+      pattern = pattern.slice(0, posX[0]) + pattern.slice(posX[0]+1);
+      console.log({t: 2, str, pattern});
+      map.set(token, bestEntry.str);
+    } else {
+      return [null, null];
+    }
+  }
+  const tester = wordPattern(pattern, str);
+
+  console.log({t: 'DONE', pattern, str, tester});
+  return [pattern, str];
+};
+
 // console.log(wordPattern('abab', "redblueredblue"));
+console.log(wordPattern('azbab', "redcblueredblue"));
 // console.log(wordPattern('abca', "onetwothreeone"));
 
 export default wordPattern;
