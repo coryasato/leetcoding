3d91dd7136f45a99b53ca28103d266b4eff72174
Cory Asato <czasato@gmail.com>
2026-02-13 13:56:38 -0800

    300: Follow up query.

--- prompts/300-longest-increasing-subsequence/index.js
+++ prompts/300-longest-increasing-subsequence/index.js
@@ -15,7 +15,7 @@
 // Input: nums = [7,7,7,7,7,7,7]
 // Output: 1
 
-const lengthOfLIS = (nums) => {
+const _lengthOfLIS = (nums) => {
   let res = [Number.MAX_SAFE_INTEGER];
 
   for (let i = 0; i < nums.length; i+=2) {
@@ -43,6 +43,60 @@ const lengthOfLIS = (nums) => {
     }
   }
 
+  return res.length;
+};
+
+// Follow up: Can you come up with an algorithm that runs in O(n log(n)) time complexity?
+
+// Divide and conquer approach.
+// Where this fails is around the conditional where we choose a chained left over a lower number right. If the theres
+// more items in the array past this branch, then the chained conditional may be wrong since there could be a greater
+// subsequence past it. We would have to open up different branches and then assess which branch has the most nums in it.
+// That wouldn't work for the time constraint.
+const lengthOfLIS = (nums) => {
+  let res = [Number.MAX_SAFE_INTEGER];
+  const set = new Set();
+
+  const recurse = (arr) => {
+    if (arr.length <= 1) {
+      return arr;
+    }
+
+    const mid = Math.floor(arr.length / 2);
+    const left = recurse(arr.slice(0, mid));
+
+    if (left.length === 1) {
+      if (left[0] < res[res.length-1]) {
+        set.delete(res[res.length-1]);
+        set.add(left[0]);
+        res = res.slice(0, -1).concat(left);
+      } else if (!set.has(left[0])) {
+        res.push(left[0]);
+        set.add(left[0]);
+      }
+    }
+
+    const right = recurse(arr.slice(mid));
+
+    if (right.length === 1) {
+      if (right[0] < res[res.length-1]) {
+        // If the last item inserted was the same stack's left, then we want to honor the left item
+        // for the "strictly increasing" logic. The chaining matters more the lesser number.
+        if (res[res.length-1] !== left[0]) {
+          set.delete(res[res.length-1]);
+          set.add(right[0]);
+          res = res.slice(0, -1).concat(right);
+        }
+      } else if (!set.has(right[0])) {
+        res.push(right[0]);
+        set.add(right[0]);
+      }
+    }
+
+    return arr;
+  };
+
+  recurse(nums);
   return res.length;
 };
 
