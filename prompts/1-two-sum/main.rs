40b3c1bb759c288519c30e8670af6004f5c5c747
Cory Asato <czasato@gmail.com>
2026-06-19 18:27:51 -0700

    1(rust): implement O(N) solution with modern Rust. remove binary

--- prompts/1-two-sum/main.rs
+++ prompts/1-two-sum/main.rs
@@ -1,32 +1,24 @@
+use std::collections::HashMap;
+
 // rustc --test main.rs && ./main --show-output
 
-// Note: First loops with Rust!
-// 1. Change the two variables to a vec. Whats the Rust best practice for nulls? We want to check if
-// 2. Using for_each is functional here and like JS is forcing us to continue looping even after we are done finding out answer.
-//    Revert to for loops and break clauses instead of a top level flag.
-pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<usize> {
-    // println!("nums: {nums:?} | target: {target}");
+pub fn two_sum(nums: Vec<i32>, target: i32) -> [i32; 2] {
+    let mut map: HashMap<i32, i32> = HashMap::new();
+    let mut res = [-1, -1];
 
-    let mut a = 0;
-    let mut b = 0;
+    for (i, num) in nums.iter().enumerate() {
+        if let Some(prev_idx) = map.get(num).copied() {
+            res = [prev_idx as i32, i as i32];
+            break;
+        }
 
-    nums.iter().enumerate().for_each(|(i, num)| {
-        println!("{num}");
+        // Builds a hashmap of remaining differences mapped to their indices. { [target - num] => idx }
+        // When a subsequent entry matches a key in the hashmap, then we have our two indices.
+        let remainder: i32 = target - num;
+        map.insert(remainder, i as i32);
+    }
 
-        nums.iter()
-            .enumerate()
-            .skip(i + 1)
-            .for_each(|(j, next_num)| {
-                // println!("{j} \\ {next_num}");
-                if (num + next_num == target) {
-                    a = i;
-                    b = j;
-                    return;
-                }
-            });
-    });
-
-    return vec![a, b];
+    return res;
 }
 
 #[cfg(test)]
@@ -35,7 +27,8 @@ mod tests {
 
     #[test]
     fn test_two_sum() {
-        assert_eq!(two_sum(vec![2, 7, 11, 15], 9), vec![0, 1]);
-        assert_eq!(two_sum(vec![3, 2, 4], 6), vec![1, 2]);
+        assert_eq!(two_sum(vec![2, 7, 11, 15], 9), [0, 1]);
+        assert_eq!(two_sum(vec![3, 2, 4], 6), [1, 2]);
+        assert_eq!(two_sum(vec![3, 3], 6), [0, 1]);
     }
 }
