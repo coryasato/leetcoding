17b450b32c4687021d2d2bada3176a2a6ff371eb
Cory Asato <czasato@gmail.com>
2026-06-23 17:30:37 -0700

    refactor(rust): 1-two-sum for more idiomatic rust. convert next prompt to better formatting

--- prompts/1-two-sum/main.rs
+++ prompts/1-two-sum/main.rs
@@ -1,24 +1,20 @@
 use std::collections::HashMap;
 
-// rustc --test main.rs && ./main --show-output
-
-pub fn two_sum(nums: Vec<i32>, target: i32) -> [i32; 2] {
-    let mut map: HashMap<i32, i32> = HashMap::new();
-    let mut res = [-1, -1];
+pub fn two_sum(nums: Vec<i32>, target: i32) -> [usize; 2] {
+    let mut map: HashMap<i32, usize> = HashMap::new();
 
     for (i, num) in nums.iter().enumerate() {
         if let Some(prev_idx) = map.get(num).copied() {
-            res = [prev_idx as i32, i as i32];
-            break;
+            return [prev_idx, i];
         }
 
         // Builds a hashmap of remaining differences mapped to their indices. { [target - num] => idx }
         // When a subsequent entry matches a key in the hashmap, then we have our two indices.
         let remainder: i32 = target - num;
-        map.insert(remainder, i as i32);
+        map.insert(remainder, i);
     }
 
-    res
+    unreachable!("The target value is guaranteed to exist in the input data");
 }
 
 #[cfg(test)]
