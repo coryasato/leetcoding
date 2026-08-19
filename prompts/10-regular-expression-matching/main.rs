50cc1f48d3c4adf1fcd9a52783fbfe2d9ae69e1b
Cory Asato <czasato@gmail.com>
2026-08-18 17:29:52 -0700

    10[refactor](rust): clean up bad logic, honor zero splats

--- prompts/10-regular-expression-matching/main.rs
+++ prompts/10-regular-expression-matching/main.rs
@@ -1,6 +1,3 @@
-// NOTE: Copying logic from the TS version and writing this for practice. I already know the logic here is
-// no bueno and I should just use two queues / vecs and emit what I can as I use up the chars / tokens.
-
 pub fn is_match(string: &str, pattern: &str) -> bool {
     if pattern == ".*" {
         return true;
@@ -18,26 +15,23 @@ pub fn is_match(string: &str, pattern: &str) -> bool {
         let ptn_char = p_vec[b];
 
         if ptn_char == '.' {
-            // Any char match, move both cursors forward
+            // Any char match, move both cursors forward.
             a += 1;
             b += 1;
         } else if ptn_char == '*' {
-            // Multi match last char
+            // Zero or more match on the last pattern char token.
             let last_ptn_char = p_vec[b.saturating_sub(1)];
 
-            // If from here on we hit a match all ".*", return early entirely.
-            if last_ptn_char == '.' {
-                break;
+            // If the current string char does not equal the last pattern char token,
+            // then the splat becomes meaningless and we should move to the next pattern token.
+            if str_char != last_ptn_char {
+                b += 1;
+                continue;
             }
 
-            // If the last seen char in ptn does not equal the current str char,
-            // then we have have a splat mismatch.
-            if last_ptn_char != str_char {
-                res = false;
-                break;
-            }
-
-            // While the current str char repeats, increment a
+            // From here the current string char equals the last pattern char token and
+            // we want to see how many forward matching chars are repeated in s_vec. We want
+            // to fast forward until the char changes or we hit the end of the vec.
             let mut temp_index = a;
             let mut temp_str_char = str_char.clone();
             while str_char == temp_str_char && temp_index < s_vec.len() {
@@ -51,31 +45,30 @@ pub fn is_match(string: &str, pattern: &str) -> bool {
             a = temp_index;
             b += 1;
         } else {
-            // Both chars must match
+            // Both chars must match.
             res = str_char == ptn_char;
             a += 1;
             b += 1;
-        }
 
-        if res == false {
-            println!("LINE 58 FLASE");
-            break;
+            // Early return from main fn.
+            if res == false {
+                return false;
+            }
         }
     }
 
-    println!("A: #{a} | B: #{b} | res: #{res}");
-    println!("A: #{} | B: #{}", s_vec.len(), p_vec.len());
+    // Past this point we either have:
+    // 1) more pattern tokens to check and only splats can be legal,
+    // 2) non-exhausted string chars, meaning not enough pattern tokens to match the input
+    // 3) or a valid match.
+    if a >= s_vec.len() && b < p_vec.len() {
+        let remaining_is_splats: bool = p_vec[b..].iter().all(|&c| c == '*');
 
-    // There are more pattern chars to consume.
-    if b < p_vec.len() {
-        if (p_vec.len() - b) > 1 {
+        if !remaining_is_splats {
             res = false;
-        } else {
-            res = p_vec[p_vec.len() - 1] == '.';
         }
     }
 
-    // There are more string chars to consume, the pattern was exhausted.
     if a < s_vec.len() {
         res = false;
     }
@@ -91,8 +84,10 @@ mod tests {
     fn test_is_match() {
         assert_eq!(is_match("aa", "a"), false);
         assert_eq!(is_match("aa", "a*"), true);
+        assert_eq!(is_match("ab", "a*"), false);
         assert_eq!(is_match("ab", ".*"), true);
         assert_eq!(is_match("ab", "abc"), false);
-        assert_eq!(is_match("abbbbbbc", "ab*c"), true);
+        assert_eq!(is_match("abc", "ab"), false);
+        assert_eq!(is_match("abbbbbbc", "ab*c*"), true);
     }
 }
